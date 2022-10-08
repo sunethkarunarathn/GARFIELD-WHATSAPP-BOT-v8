ad8c34e1fd33a7263cf9c19723a0cc3041ef3bd1
sunethkarunarathn <115002382+sunethkarunarathn@users.noreply.github.com>
2022-10-08 17:27:26 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -13,7 +13,7 @@ global.APIKeys = {
 	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 // 👈 You Can change this your choice
-global.emoji01 = ['➢֒'] // 👈 Enter your Emoji 
+global.emoji01 = ['➢֒👼'] // 👈 Enter your Emoji 
 global.emoji02 = ['〠☠'] //👈 Enter your Emoji
 global.notice = ` *හායි කෝමද ඉතින්☺*
 *SUNETH*
